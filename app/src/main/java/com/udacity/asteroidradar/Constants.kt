05b0c83291b7c04f99869121b5042e673838999a
maria.rojasmedrano <maria.rojasmedrano@gmail.com>
2022-03-31 10:47:09 +0200

    change in constants

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -3,7 +3,7 @@ package com.udacity.asteroidradar
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
-    const val API_KEY = "5fwqNoH4eJFKBrqGclP6hGzYLuZHLerYuG1L4Kaq"
+    const val API_KEY = ""
     const val BASE_URL = "https://api.nasa.gov/"
     const val DEFAULT_IMAGE_URL = "https://apod.nasa.gov/apod/image/2001/STSCI-H-p2006a-h-1024x614.jpg"
     const val DEFAULT_IMAGE_TITLE = "Nasa default image"
